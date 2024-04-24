55a1f463727e94895b8076d4564d61aac7ab2836
amsmi1 <andrea.m.smith1@aexp.com>
2024-04-24 16:52:00 -0400

    New version of number game and scratch pad work from book

---
+++ numberv1.py
@@ -0,0 +1,27 @@
+import random
+
+
+user_input = int(input("Guess a number between 1 and 10: "))
+computer_number = random.randint(1, 10)
+number_guess = int(input("How many chances do you think you need? "))
+
+
+def game_play(user_input, computer_number, number_guess):
+    count = 0
+    while number_guess > count:
+        if computer_number == user_input:
+            print("You win! Good job!")
+        elif computer_number > user_input:
+            count += 1
+            number_guess -= 1
+            print(f"Too low! You have {number_guess} left!  ")
+            user_input = int(input("Guess a number between 1 and 10: "))
+        elif computer_number < user_input:
+            count += 1
+            number_guess -= 1
+            print(f"Too high! you have {number_guess} left! ")
+            user_input = int(input("Guess a number between 1 and 10: "))
+    print(f"Out of guesses! The number was {computer_number}.  Try again! ")
+
+
+
--- pythonquickstart_chapter1.py
+++ pythonquickstart_chapter1.py
@@ -54,3 +54,15 @@ temps = [
 singular_words = ['student', 'teacher', 'room']
 for word in singular_words:
     print(word + 's')
+else:
+    print("Done!")
+
+# The planets
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+# Display the planet and its number
+for index, value in enumerate(planets):
+    print("Planet " + str(index) + ": " + value)
+
+for index, value in enumerate(planets):
+    print("Planet " + str(index + 1) + ": " + value)
