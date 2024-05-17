ab1cbe0d533215187f8ece539e8636dc9767e1e4
DreaSmizz <a.smith102781@gmail.com>
2024-05-17 11:42:51 -0400

    Powershell scripting

--- powershell/first.ps1
+++ powershell/first.ps1
@@ -1,1 +1,11 @@
-function Get-time {get-date -Format hh:mm}
+# function Get-time {get-date -Format hh:mm}
+
+# $Name = "Print Spooler"
+# $Service = Get-Service -display $Name -ErrorAction silentlycontinue
+# If (-Not $Service) {$Name + " is not installed on this computer."}
+# Else {$Name + " is installed."
+# $Name + ":s status is: " + $Service.status }
+
+# First Basic Script
+
+Get-CimInstance -ClassName Win32_logcialdisk -Filter "DeviceID='C:'"
