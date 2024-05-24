a461f9bdb457daa28d28a66824e7613015b6ba66
DreaSmizz <a.smith102781@gmail.com>
2024-05-23 20:18:44 -0400

    Updated code

--- cribl.py
+++ cribl.py
@@ -12,40 +12,81 @@
 import json
 import csv
 import requests
+import pandas as pd
 from splunk_data_sender import SplunkSender
-import pandas
 
-splunk_conf = {
-    #'endpoint' = "https://default.main.infallible-miatselski-783se76.cribl.cloud:10200",
-    #'endpoint':'https://http-input-prd-p-t9vwy.splunkcloud.com:8088/services/collector',
-    #hec_token = 'e8983479-46a4-5817-aa6e-e0251328bc79'
-    #'endpoint':'si-i-0403e82113a9640bd.prd-p-t9vwy.splunkcloud.com',
-    'endpoint':'http://192.168.4.198',
-    'port':'9000',
-    'token':'424e510f-631d-4da3-b0d6-bbcdc15fb91a',
-}
 
-splunk = SplunkSender(**splunk_conf)
+
+#splunk = SplunkSender(**splunk_conf)
+
+# Send requests for CSV file
+stock = pd.read_csv("https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv")
+stocks_json = stock.to_json(orient='records')
+# My lab splunk 
+url = 'http://localhost:8000'
+data = stocks_json
+splunk_headers = {'Authorization': 'Splunk 424e510f-631d-4da3-b0d6-bbcdc15fb91a','Content-Type': 'application/json'}
+response = requests.post(url, headers=splunk_headers, data=json.dumps(data), verify=False )
+print(response.status_code)
+#print(response.content)
+
+
+
+
+
+#splunk_conf = {
+#    'endpoint':'http://localhost',
+#    'port':'8000',
+#    'token':'424e510f-631d-4da3-b0d6-bbcdc15fb91a',
+#}
+
+
+#connection = httplib2.HTTPSConnection('localhosts:8088')
+#connection.request('POST', url, json.dumps(data), headers)
+
+#splunk = SplunkSender(**splunk_conf)
 
 #cribl_stream = 'https://default.main.infallible-miatselski-783se76.cribl.cloud:10200/api/v1/system/outputs/infallible-miatselski-783se76'
 #cribl_stream_url = 'http://192.168.4.198:9000/'
+#response = requests.posts('https://192.168.4.198:900/api/v1', json=stocks_json)
+#curl -X POST https://192.168.4.198:9000/api/v1/auth/login -H "Content-Type: stocks_json" -d '{"username":"admin","password":"J@deNyla2018"}'
 
 
-# Send requests for CSV file
-stock_csv = requests.get('https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv')
+
+
+
+
+
+
+
+
+
+#stock_csv = requests.get('https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv')
+#url = 'https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv'
+#jsondict=json.loads(stocks_json)
+#first = list(jsondict)[0]
+#print(first)
+
+
 # If dataset no longer available, send error message
-if stock_csv.status_code == 200:
-    csv_content = stock_csv.content
-    csv_reader = csv.DictReader(csv_content)
-    #stocks = pandas.read_csv('csv_content')
+#if stock_csv.status_code == 200:
+#    csv_content = stock_csv.content
+#    print(csv_content)
+    #csv_file = open('stock.csv', 'wb')
+#    csv_reader = csv.DictReader(BytesIO(csv_content))
+#    json_data = json.dumps(list(csv_reader))
+#    print(json_data)
+    #csv_file.write(csv_content)
 
-#json_stock_data = json.dumps(stocks, indent=4)
-#print(json_stock_data)
-    csv_file = open('stock.csv', 'wb')
-    csv_file.write(csv_content)
-    csv_file.close()
+
+    #csv_file.close()
         
 
+
+
+#stocks = pandas.read_csv('csv_content')
+#json_stock_data = json.dumps(stocks, indent=4)
+#print(json_stock_data)
 #print(stock_csv.status_code)
 # Convert CSV data to JSON so it can be ingested
 #with open('stock_csv', mode='r') as file:
