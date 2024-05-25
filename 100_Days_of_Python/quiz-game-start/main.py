11f91255eec9cbe971438ff3903ed3c85cd72f2a
DreaSmizz <a.smith102781@gmail.com>
2024-05-25 17:06:33 -0400

    moved code

---
+++ 100_Days_of_Python/quiz-game-start/main.py
@@ -0,0 +1,15 @@
+#Question object will have two attributes, text and answer
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+
