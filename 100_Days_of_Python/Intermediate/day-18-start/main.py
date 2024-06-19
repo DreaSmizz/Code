42d2fe97f9602bc0aaea4a2166a70b412b225340
DreaSmizz <a.smith102781@gmail.com>
2024-06-19 11:20:04 -0400

    Adding new code

--- 100_Days_of_Python/Intermediate/day-18-start/main.py
+++ 100_Days_of_Python/Intermediate/day-18-start/main.py
@@ -1,17 +1,27 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
+from turtle import Screen
 
+tim = t.Turtle()
+t.colormode(255)
+#t_turtle.shape("turtle")
+#t_turtle.color('green')
 
-t_turtle = Turtle()
-t_turtle.shape("turtle")
-t_turtle.color('green')
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
 for _ in range(200):
-    t_turtle.forward(30)
-    t_turtle.setheading(random.choice(directions))
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 # Generate a random walk
 #def random_walk()
